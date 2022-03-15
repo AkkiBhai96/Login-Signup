8d4d71093773702455749a3df7b89d41178a1d9e
AkkiBhai96 <55734383+AkkiBhai96@users.noreply.github.com>
2022-03-15 12:51:13 +0530

    Card View Adjusted

--- src/App.js
+++ src/App.js
@@ -45,44 +45,46 @@ export default function App() {
           </div>
         </nav>
       </div>
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src={logo} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+      <div class="container-fluid card-container">
+        <div class="card-group">
+          <div class="card">
+            <img class="card-img-top" src={logo} alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
-        </div>
-        <div class="card">
-          <img class="card-img-top" src={logo} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+          <div class="card">
+            <img class="card-img-top" src={logo} alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
-        </div>
-        <div class="card">
-          <img class="card-img-top" src={logo} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
+          <div class="card">
+            <img class="card-img-top" src={logo} alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
         </div>
       </div>
